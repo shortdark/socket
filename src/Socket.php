c919b3621971690564f2ec97644f6fa4745c0b19
Neil <neil@shortdark.net>
2021-08-25 22:30:27 +0100

    Update: Shrinking data so that everything fits onto the graph uses the full graph width. New version.

--- src/Socket.php
+++ src/Socket.php
@@ -3,7 +3,7 @@
 /**
  * Socket - takes data as an array and plots it as a SVG graph.
  * PHP Version >= 7.0
- * Version 0.2.02
+ * Version 0.2.03
  * @package Socket
  * @link https://github.com/shortdark/socket/
  * @author Neil Ludlow (shortdark) <neil@shortdark.net>
